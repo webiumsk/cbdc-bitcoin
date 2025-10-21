283e986c1a7748ac5b4a0796c8e2de10e21ee6ab
webiumsk <info@webium.sk>
2025-10-21 23:27:08 +0200

    fix locale and meta

--- src/App.vue
+++ src/App.vue
@@ -20,12 +20,13 @@ const route = useRoute()
 const siteData = computed(() => {
   const baseUrl = 'https://cbdc.icu'
   const currentLocale = locale.value
+  const ogImage = 'https://cbdc.icu/cbdc-vs-btc.jpg'
   let url = baseUrl
-  let ogLocale = 'sk_SK'
+  let ogLocale = 'en_US' // Default EN
   
-  if (currentLocale === 'en') {
-    url = `${baseUrl}/en`
-    ogLocale = 'en_US'
+  if (currentLocale === 'sk') {
+    url = `${baseUrl}/sk`
+    ogLocale = 'sk_SK'
   } else if (currentLocale === 'es') {
     url = `${baseUrl}/es`
     ogLocale = 'es_ES'
@@ -38,30 +39,34 @@ const siteData = computed(() => {
     title: t('meta.title'),
     meta: [
       { name: 'description', content: t('meta.description') },
-      { name: 'keywords', content: 'CBDC, Bitcoin, digitálne euro, ECB, finančná sloboda, decentralizácia, kryptomeny, digital euro, financial freedom, decentralization, cryptocurrency, euro digital, digitaler euro' },
+      { name: 'keywords', content: 'CBDC, Bitcoin, digital euro, ECB, financial freedom, decentralization, cryptocurrency, digitálne euro, finančná sloboda, kryptomeny, euro digital, digitaler euro' },
       
       // Open Graph
       { property: 'og:type', content: 'website' },
+      { property: 'og:image', content: ogImage },
+      { property: 'og:site_name', content: 'CBDC.icu' },
       { property: 'og:url', content: url },
       { property: 'og:title', content: t('meta.ogTitle') },
       { property: 'og:description', content: t('meta.ogDescription') },
-      { property: 'og:image', content: 'https://cbdc.icu/cbdc-vs-btc.jpg' },
+      { property: 'og:locale', content: ogLocale },
+      { property: 'og:image:secure_url', content: ogImage },
+      { property: 'og:image:type', content: 'image/jpeg' },
       { property: 'og:image:width', content: '1200' },
       { property: 'og:image:height', content: '630' },
       { property: 'og:image:alt', content: t('meta.imageAlt') },
-      { property: 'og:locale', content: ogLocale },
       
-      // Twitter
+      // Twitter Card
       { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:url', content: url },
       { name: 'twitter:title', content: t('meta.ogTitle') },
       { name: 'twitter:description', content: t('meta.ogDescription') },
-      { name: 'twitter:image', content: 'https://cbdc.icu/cbdc-vs-btc.jpg' },
+      { name: 'twitter:image', content: ogImage },
+      { name: 'twitter:image:alt', content: t('meta.imageAlt') },
     ],
     link: [
       { rel: 'canonical', href: url },
-      { rel: 'alternate', hreflang: 'sk', href: baseUrl },
-      { rel: 'alternate', hreflang: 'en', href: `${baseUrl}/en` },
+      { rel: 'alternate', hreflang: 'en', href: baseUrl },
+      { rel: 'alternate', hreflang: 'sk', href: `${baseUrl}/sk` },
       { rel: 'alternate', hreflang: 'es', href: `${baseUrl}/es` },
       { rel: 'alternate', hreflang: 'de', href: `${baseUrl}/de` },
       { rel: 'alternate', hreflang: 'x-default', href: baseUrl },
